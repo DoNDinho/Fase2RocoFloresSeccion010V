b71c0226c451f26bcd1a31547b6da943cb37a737
Diego Flores <di.floresr@ibm.com>
2020-10-27 21:01:10 -0300

    playstore: se crea modelo de articulos, y se crea super usuario

--- articulos/admin.py
+++ articulos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tipo_Articulo, Articulo
+
+admin.site.register(Tipo_Articulo)
+admin.site.register(Articulo)
--- articulos/models.py
+++ articulos/models.py
@@ -1,2 +1,26 @@
 from django.db import models
 
+class Tipo_Articulo(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50, help_text="Añada descripción del tipo artículo")
+
+    class Meta:
+        ordering=['descripcion']
+
+    def __str__(self):
+        return self.descripcion
+
+
+class Articulo(models.Model):
+    #id 
+    tipoArticulo = models.ForeignKey(Tipo_Articulo, on_delete=models.CASCADE) 
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    img = models.ImageField(upload_to='img/', null=True, blank=True)
+    stock = models.IntegerField()
+
+    class Meta:
+        ordering=['nombre']
+
+    def __str__(self):
+        return self.nombre
