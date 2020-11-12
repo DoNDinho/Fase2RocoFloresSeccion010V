98019d9c14b3adc59ede8f81ecb9949520ffedd2
Diego Flores <di.floresr@ibm.com>
2020-11-12 20:31:23 -0300

    finalización del proyecto

--- articulos/tests.py
+++ articulos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+from .models import Articulo
+
+class ArticuloModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        Articulo.objects.create(tipoArticulo=1, nombre='MK 11', precio=15000, img='null', stock=23)
+
+    def test_first_name_label(self):
+        articulo=Articulo.objects.get(id=1)
+        field_label = articulo._meta.get_field('nombre').verbose_name
+        self.assertEquals(field_label,'nombre')
